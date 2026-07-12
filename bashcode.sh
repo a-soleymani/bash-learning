2579914be79b5be1c24d8dd043d690402e730d7c
Aliasghar Soleymani <slywork555@gmail.com>
2026-07-12 01:26:51 -0700

    Update bashcode.sh

--- bashcode.sh
+++ bashcode.sh
@@ -1,33 +1,60 @@
 #!/bin/bash
 
 echo -n "Enter First Number: "
-read NUM1
+read num1
 
 echo -n "Enter Second Number: "
-read NUM2
+read num2
 
 echo -n "Enter Third Number: "
-read NUM3
+read num3
 
 
-if [ $NUM1 -gt $NUM2 ]
+
+if [ $num1 -gt $num2 ] && [ $num1 -gt $num3 ]
 then
-	if [ $NUM1 -gt $NUM3 ]
-	then
-		echo "$NUM1 is the Largest"
-	elif [ $NUM1 -lt $NUM3 ]
-	then
-		echo "$NUM3 is the Largest"
-	fi
-elif [ $NUM2 -gt $NUM3 ]
-then
-	if [ $NUM1 -eq $NUM2 ]
-	then
-		echo "$NUM1 & $NUM2 are same and largest"
-	else
-		echo "$NUM2 is largest"
-	fi
+	echo "$num1 is largest"
 
-else
-	echo "$NUM1 & $NUM2 & $NUM3 are same"
-fi	
+
+elif [ $num2 -gt $num1 ] && [ $num2 -gt $num3 ]
+then
+	echo "$num2 is largest"
+
+
+elif [ $num3 -gt $num1 ] && [ $num3 -gt $num2 ]
+then
+	echo "$num3 is largest"
+
+elif [ $num1 -eq $num2 ] && [ $num1 -eq $num3 ]
+then
+	echo "All three are equal"
+
+
+elif [ $num1 -eq $num2 ] && [ $num1 -gt $num3 ]
+then
+	echo "$num1 & $num2 are equal and largest"
+
+elif  [ $num1 -eq $num2 ] && [ $num1 -lt $num3 ]
+then
+	echo "$num1 & $num2 are equal and smaller"
+	
+
+
+elif [ $num2 -eq $num3 ] && [ $num2 -gt $num1 ]
+then
+	echo "$num2 & $num3 are equal and largest"
+
+elif [ $num2 -eq $num3 ] && [ $num2 -lt $num1 ]
+then
+	echo "$num2 & $num3 are equal and samller"
+
+
+elif [ $num1 -eq $num3 ] && [ $num1 -gt $num2 ]
+then
+	echo "$num1 & $num3 are equal and largest"
+
+elif [ $num1 -eq $num3 ] && [ $num1 -lt $num2 ]
+then
+	echo "$num1 & $num3 are equal and smaller"
+	
+fi
